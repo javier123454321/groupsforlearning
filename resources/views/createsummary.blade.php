5a99585801f5543144187abb61ae05b4ed668e34
JavierGonzalez <javier.e.glz@gmail.com>
2021-03-19 11:22:37 -0500

    linting fix

--- resources/views/createsummary.blade.php
+++ resources/views/createsummary.blade.php
@@ -1,9 +1,7 @@
 <x-app-layout>
-
-<section>
-<div class="w-full px-8 py-4 my-4 bg-white rounded-lg shadow-lg" x-data="{showcreatebox: false}">
-    <livewire:create-thread week="1"/>
-</div>
-</section>
-
+    <section>
+        <div class="w-full px-8 py-4 my-4 bg-white rounded-lg shadow-lg" x-data="{showcreatebox: false}">
+            <livewire:create-thread week="1" />
+        </div>
+    </section>
 </x-app-layout>
