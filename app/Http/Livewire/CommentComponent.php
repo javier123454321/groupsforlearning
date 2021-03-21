5d475c8b5a7fb9df4ffe4011695fc6d1e1175485
JavierGonzalez <javier.e.glz@gmail.com>
2021-03-20 22:58:10 -0500

    allow replies to comments

--- app/Http/Livewire/CommentComponent.php
+++ app/Http/Livewire/CommentComponent.php
@@ -10,8 +10,10 @@ class CommentComponent extends Component
 {
     public $comment;
     public $children;
+    public $reply;
     protected $rules = [
         'comment.body' => 'required|text',
+        'reply' => 'reuired|text'
     ];
     public function render()
     {
@@ -20,6 +22,20 @@ class CommentComponent extends Component
     public function mount()
     {
         $this->children = Comment::where("parent_comment", $this->comment->id)->orderBy('created_at', 'DESC')->get()->all();
+    }
+    public function reply()
+    {
+        $comment = new Comment();
+        $comment["parent_comment"] = $this->comment->id;
+        $comment["body"] = $this->reply;
+        $comment["thread_id"] = $this->comment->thread_id;
+        $comment["user_id"] = auth()->user()->id;
+        if($comment->save())
+        {
+            $this->reply = '';
+            array_unshift($this->children, $comment);
+            $this->dispatchBrowserEvent('replied-to-comment');
+        }
     }
     public function update()
     {
--- resources/views/livewire/comment-component.blade.php
+++ resources/views/livewire/comment-component.blade.php
@@ -1,27 +1,39 @@
 <div>
-    <div class="p-4 mt-2 text-left rounded-lg bg-purple-50" x-data="{editing: false, body: '{{ $comment['body'] }}'}" x-on:comment-edited.window="editing = false">
+    <div class="p-4 mt-2 text-left rounded-lg bg-purple-50"
+        x-data="{editing: false, replying:false, body: '{{ $comment['body'] }}'}"
+        x-on:comment-edited.window="editing = false">
         <p x-show.transition="!editing">{{ $comment['body'] }}</p>
         <div x-show.transition="editing">
-            <textarea class="w-full px-2 bg-gray-100 border border-purple-300 rounded-lg" :value="'{{$comment['body']}}'"
-                placeholder="Reply..." wire:model.defer="comment.body"></textarea>
+            <textarea class="w-full px-2 bg-gray-100 border border-purple-300 rounded-lg"
+                :value="'{{ $comment['body'] }}'" placeholder="Reply..." wire:model.defer="comment.body"></textarea>
         </div>
         <div class="flex justify-between text-sm text-gray-800 font-extralight">
             <div>
-            By: {{ isset($comment['user']['name']) ? $comment['user']['name'] : 'unknown' }}
-            @if ($comment['user'] == Auth::user())
-                <button x-on:click="editing = true" x-show.transition="!editing"
+                By: {{ isset($comment['user']['name']) ? $comment['user']['name'] : 'unknown' }}
+                @if ($comment['user'] == Auth::user())
+                    <button x-on:click="editing = true" x-show.transition="!editing"
+                        class="w-6 h-6 ml-4 text-sm leading-7 text-purple-800 text-opacity-75 cursor-pointer hover:text-opacity-100">
+                        edit
+                    </button>
+                @endif
+                <button x-on:click="replying = !replying"
                     class="w-6 h-6 ml-4 text-sm leading-7 text-purple-800 text-opacity-75 cursor-pointer hover:text-opacity-100">
-                    edit
+                    reply
                 </button>
-            @endif
             </div>
             <div x-show.transition="editing">
                 <button class="w-24 px-4 py-2 mr-2 bg-gray-200 rounded" x-on:click="editing = false">Cancel</button>
                 <button
                     class="w-24 px-4 py-2 text-white bg-purple-700 rounded disabled:opacity-50 disabled:text-gray-50"
-                    wire:click="update()"
-                    type="submit">Save</button>
+                    wire:click="update()" type="submit">Save</button>
             </div>
+        </div>
+        <div x-show.transition="replying" x-on:replied-to-comment.window="replying = false">
+            <textarea class="w-full px-2 mt-2 bg-gray-100 border border-purple-300 rounded-lg" wire:model="reply"
+                placeholder="Reply..." wire:model.defer="reply.body"></textarea>
+            <button class="w-24 px-4 py-2 mr-2 bg-gray-200 rounded" x-on:click="replying = false">Cancel</button>
+            <button class="w-24 px-4 py-2 text-white bg-purple-700 rounded disabled:opacity-50 disabled:text-gray-50"
+                wire:click="reply()" type="submit">Save</button>
         </div>
         <div class="border-l-2">
             @foreach ($children as $key => $child)
