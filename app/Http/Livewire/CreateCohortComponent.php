d33cd6af44a6b77340031e7ad8d68456e250d967
JavierGonzalez <javier.e.glz@gmail.com>
2021-03-21 23:04:22 -0500

    allow to create a cohort

--- app/Http/Livewire/CreateCohortComponent.php
+++ app/Http/Livewire/CreateCohortComponent.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Cohort;
 use Livewire\Component;
 
 class CreateCohortComponent extends Component
 {
+    public $displayName;
+    public $course;
+    public $courseUrl;
+    protected $rules = [
+        'displayName' => 'required|text',
+        'course' => 'required|text',
+    ];
+
     public function render()
     {
         return view('livewire.create-cohort-component');
+    }
+    public function save()
+    {
+        $cohort = new Cohort();
+        $cohort["display_name"] = $this->displayName;
+        $cohort["course"] = $this->course;
+        $cohort["slug"] = str_replace(" ", "-", $this->displayName);
+        $cohort->save();
     }
 }
--- resources/views/livewire/create-cohort-component.blade.php
+++ resources/views/livewire/create-cohort-component.blade.php
@@ -1,5 +1,35 @@
-<x-app-layout>
-    <div>
-        test
-    </div>
-</x-app-layout>
+    <form wire:submit.prevent="save" class="flex flex-col">
+        <label for="cohort-name">Cohort Name</label>
+        <input wire:model="displayName" type="text" id="cohort-name"
+            class="mb-5 border border-purple-300 rounded-lg focus:bg-purple-50 focus:border-purple-500"
+            autocomplete="off">
+        <div x-data="{showTooltip: false}" class="flex">
+            <label for="course-title">Course Title</label>
+            <div class="relative ml-3 float">
+                <span x-on:mouseover="showTooltip = true" x-on:focus="showTooltip = true"
+                    x-on:mouseout="showTooltip = false" x-on:blur="showTooltip = false" tabindex="0"
+                    class="px-2 text-purple-900 border border-purple-900 rounded-full cursor-pointer bg-purple-50">i</span>
+                <div x-show="showTooltip" class="absolute p-2 text-white transform translate-x-6 -translate-y-full bg-gray-800 w-96 rounded-xl bg-opacity-90">
+                    Optional, if your cohort is based around a course, what is the course name?
+                </div>
+            </div>
+        </div>
+        <input wire:model="course" type="text" id="course-title"
+            class="mb-5 border border-purple-300 rounded-lg focus:bg-purple-50 focus:border-purple-500"
+            autocomplete="off">
+        <div x-data="{showTooltip: false}" class="flex">
+            <label for="course-url">Link To Course</label>
+            <div class="relative ml-3 float">
+            <span x-on:mouseover="showTooltip = true" x-on:focus="showTooltip = true"
+                x-on:mouseout="showTooltip = false" x-on:blur="showTooltip = false" tabindex="0"
+                class="px-2 text-purple-900 border border-purple-900 rounded-full cursor-pointer bg-purple-50">i</span>
+                <div x-show="showTooltip" class="absolute p-2 text-white transform translate-x-6 -translate-y-full bg-gray-800 w-96 rounded-xl bg-opacity-90">
+                    Optional, if your cohort is based around a course, write down the course url here
+                </div>
+                </div>
+        </div>
+        <input wire:model="courseUrl" type="url" id="course-url"
+            class="mb-5 border border-purple-300 rounded-lg focus:bg-purple-50 focus:border-purple-500"
+            autocomplete="off">
+        <input type="submit">
+    </form>
