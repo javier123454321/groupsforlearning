c5a03be6600306ffa3d84ee35e90673805558890
JavierGonzalez <javier.e.glz@gmail.com>
2021-03-24 21:42:23 -0500

    add a cohort comoponent test

---
+++ tests/Feature/Livewire/JoinCohortComponentTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Feature\Livewire;
+
+use App\Http\Livewire\JoinCohortComponent;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+use Livewire\Livewire;
+
+class JoinCohortComponentTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * The component renders a button if the user is not part of that cohort
+     *
+     * @return void
+     */
+    public function test_a_user_sees_a_button_to_join_a_cohort()
+    {
+        $this->actingAs(\App\Models\User::factory()->create());
+        $cohort = \App\Models\Cohort::factory()->create();
+        Livewire::test(JoinCohortComponent::class, [
+            "cohort" => $cohort
+        ])
+            ->assertSee("Join")
+            ->assertDontSee("You are a member");
+    }
+    /**
+     * The component doesn't render the button if the user is part of that cohort
+     *
+     * @return void
+     */
+    public function test_a_member_does_not_see_a_button_to_join_a_cohort()
+    {
+        $user = \App\Models\User::factory()->create();
+        $this->actingAs($user);
+        $cohort = \App\Models\Cohort::factory()->create();
+        DB::table("cohort_user")->insert([
+                "cohort_id" => $cohort->id,
+                "user_id" => $user->id
+                ]);
+        Livewire::test(JoinCohortComponent::class, [
+            "cohort" => $cohort
+        ])
+            ->assertDontSee("Join")
+            ->assertSee("You are a member");
+    }
+    /**
+     * When pressing join, a join request gets sent to for the cohort
+     *
+     * @return void
+     */
+    public function test_a_member_does_not_see_a_button_to_join_a_cohort()
+    {
+        $user = \App\Models\User::factory()->create();
+        $this->actingAs($user);
+        $cohort = \App\Models\Cohort::factory()->create();
+        Livewire::test(JoinCohortComponent::class, [
+            "cohort" => $cohort
+        ])
+            ->call("join");
+        $this->assertTrue(DB::)
+    }
+}
