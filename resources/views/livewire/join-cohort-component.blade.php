1022beecb85be607048a69c8521443b3b6ea4324
JavierGonzalez <javier.e.glz@gmail.com>
2021-03-28 18:59:49 -0500

    styling and layout

--- resources/views/livewire/join-cohort-component.blade.php
+++ resources/views/livewire/join-cohort-component.blade.php
@@ -1,8 +1,9 @@
-<div>
+<div class="relative">
   @if(!$cohort->users->contains( auth()->user()->id ))
     <button>Join</button>
   @else
-    You are a member
+    <div class="bg-gray-300 p-2 rounded placeholder-blue-200 w-40 text-center">
+      You are a member
+    </div>
   @endif
-
 </div>
