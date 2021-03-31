fadca5af3b610488aed2f66329ed42a8f0ae936a
JavierGonzalez <javier.e.glz@gmail.com>
2021-03-30 20:43:59 -0500

    add user to thread model

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -13,4 +13,9 @@ class Thread extends Model
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
