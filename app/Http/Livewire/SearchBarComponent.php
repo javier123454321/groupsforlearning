df11644219865da07e7fb7e54b0aaa7cb7f93e14
JavierGonzalez <javier.e.glz@gmail.com>
2021-05-18 20:14:25 -0500

    add search to nav

---
+++ app/Http/Livewire/SearchBarComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SearchBarComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.search-bar-component');
+    }
+}
---
+++ resources/views/livewire/search-bar-component.blade.php
@@ -0,0 +1,5 @@
+<div class="flex items-center">
+  <input 
+    type="text" 
+    class="transition duration-300 rounded md:mr-4 focus:ring-4 focus:ring-purple-600 focus:ring-opacity-20 border border-gray-400">
+</div>
